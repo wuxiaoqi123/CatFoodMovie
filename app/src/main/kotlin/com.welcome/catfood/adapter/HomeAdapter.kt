7b81784af9c295a0f314a4efde85a7515ebc35f9
武小奇 <1321972760@qq.com>
2018-12-20 23:50:07 +0800

    增加视频页

--- app/src/main/kotlin/com.welcome/catfood/adapter/HomeAdapter.kt
+++ app/src/main/kotlin/com.welcome/catfood/adapter/HomeAdapter.kt
@@ -2,6 +2,7 @@ package com.welcome.catfood.adapter
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import cn.bingoogolapple.bgabanner.BGABanner
@@ -9,6 +10,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.welcome.catfood.R
+import com.welcome.catfood.activity.VideoDetailActivity
 import com.welcome.catfood.bean.HomeBean
 import com.welcome.catfood.extend.durationFormat
 import io.reactivex.Observable
@@ -157,6 +159,7 @@ class HomeAdapter(context: Context, data: ArrayList<HomeBean.Issue.Item>) :
     }
 
     private fun goToVideoPlayer(activity: Activity, view: View, itemData: HomeBean.Issue.Item) {
-        //TODO
+        val intent = Intent(activity, VideoDetailActivity::class.java)
+        activity.startActivity(intent)
     }
 }
