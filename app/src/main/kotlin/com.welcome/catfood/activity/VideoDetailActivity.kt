c460e69e0ebf74783411c665f4669b33b6794c7e
武小奇 <1321972760@qq.com>
2018-12-20 23:50:26 +0800

    增加视频页

---
+++ app/src/main/kotlin/com.welcome/catfood/activity/VideoDetailActivity.kt
@@ -0,0 +1,25 @@
+package com.welcome.catfood.activity
+
+import com.welcome.catfood.R
+import com.welcome.catfood.base.BaseActivity
+import com.welcome.catfood.base.IBasePresenter
+
+/**
+ * <pre>
+ *     author : wuxiaoqi
+ *     e-mail : 1321972760@qq.com
+ *     time   : 2018/12/20
+ *     desc   : 视频页
+ *     version: 1.0
+ * </pre>
+ */
+class VideoDetailActivity : BaseActivity<IBasePresenter>() {
+
+    override fun layoutId(): Int = R.layout.activity_video_detail
+
+    override fun initView() {
+    }
+
+    override fun startRequest() {
+    }
+}
