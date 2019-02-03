9cc02bec6c6edcc9f104aaf73688e3030096f2fb
武小奇 <1321972760@qq.com>
2019-02-03 10:30:08 +0800

    设置

---
+++ app/src/main/kotlin/com.welcome/catfood/manager/SettingManager.kt
@@ -0,0 +1,23 @@
+package com.welcome.catfood.manager
+
+/**
+ * <pre>
+ *     author : wuxiaoqi
+ *     e-mail : 1321972760@qq.com
+ *     time   : 2019/02/03
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+object SettingManager {
+
+    // wifi 连网
+    val SET_WIFI_CONNECT = "set_wifi_connect"
+
+    // 主题颜色
+    val THEME_COLOR = "theme_color"
+
+    // 夜间模式
+    val HIGHT_MODEL = "hight_model"
+
+}
